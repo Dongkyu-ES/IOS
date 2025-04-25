6b32929607f3d012bbe966ab6cf76f11dc68d3c8
Dominic <ciuizz@naver.com>
2025-04-25 12:10:08 +0900

    NavigationPath doing

--- APP1_voiceRecorder/beginning/voiceMemo/voiceMemo/Model/Onboarding/OnboardingContent.swift
+++ APP1_voiceRecorder/beginning/voiceMemo/voiceMemo/Model/Onboarding/OnboardingContent.swift
@@ -5,5 +5,18 @@
 
 import Foundation
 
-struct OnboardingContent {
+struct OnboardingContent: Hashable {
+    var imageFileName: String
+    var title: String
+    var subtitle: String
+    
+    init(
+        imageFileName: String,
+        title: String,
+        subtitle: String
+    ) {
+        self.imageFileName = imageFileName
+        self.title = title
+        self.subtitle = subtitle
+    }
 }
